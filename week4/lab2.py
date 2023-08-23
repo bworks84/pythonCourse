fe32badb1f11b5b4e50b55e7c5419b8db9947ab5
robworks <bworks84@gmail.com>
2023-08-23 15:10:42 -0400

    Week 2 lab

---
+++ week4/lab2.py
@@ -0,0 +1,43 @@
+import sys
+
+calcmessage = input(
+    "Please enter a simple calculation using two numbers and an arithmetic operator\n")
+parameters = calcmessage.split()
+
+val1 = float(parameters[0])
+operator = parameters[1]
+val2 = float(parameters[2])
+
+
+# def compare_calc():
+#     if operator == "+":
+#         result = val1 + val2
+#         return result
+#     elif operator == "-":
+#         result = val1 - val2
+#         return result
+#     elif operator == "*":
+#         result = val1 * val2
+#         return result
+#     else:
+#         result = val1 / val2
+#         return result
+
+
+# print(compare_calc())
+
+##############################
+# switch statement
+# required installing python@3.10
+# followed directions from freecodecamp listed in assignment
+print(sys.version)
+
+match operator:
+    case "+":
+        result = val1 + val2
+    case "-":
+        result = val1 - val2
+    case "*":
+        result = val1 * val2
+    case _:
+        result = val1 / val2
